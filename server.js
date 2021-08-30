5322f35806039eb3cde49af3bafc250de66ff46a
Patrick Igwe <patrickoigwe@gmail.com>
2021-08-31 00:57:20 +0400

    new cjanges

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const FAVORITE_INSTRUCTOR = process.env.name
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,7 +7,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
